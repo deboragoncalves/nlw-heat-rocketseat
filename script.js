68af73916054d14aab30e2eb7b8776ca19ab5ade
Débora Cristina Pereira Gonçalves <deboracristinapereira123@gmail.com>
2021-10-23 22:31:59 -0300

    Consumir API Github

--- script.js
+++ script.js
@@ -17,14 +17,35 @@ setLinksSocialMedia = () => {
         // pegar filhos da li: li.children. 0 é o primeiro: a
         // userSocialMedia[liClass] = userSocialMedia.github, .youtube, .facebook
 
-        li.children[0].href = "https://www." + liClass + ".com/" + userSocialMedia[liClass];
-    }
-
-    let divGithub = document.getElementById("divGithub");
-
-    for (let a of divGithub.children) {
-        a.href = "https://www.github.com/" + userSocialMedia.github;
+        li.children[0].href = `https://www.${liClass}.com/${userSocialMedia[liClass]}`;        
     }
 };
 
 setLinksSocialMedia();
+
+getInformationsGithub = () => {
+    const url = `https://api.github.com/users/${userSocialMedia.github}`;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {        
+        let userName = document.getElementById("name");
+        userName.textContent = data.name;
+
+        let userGithub = document.getElementById("userGithub");
+        userGithub.textContent = data.login;
+
+        let linkUserGithub = document.getElementById("linkUserGithub");
+        linkUserGithub.href = data.html_url;
+
+        let imageGithub = document.getElementById("imageGithub");
+        imageGithub.href = data.html_url;
+
+        let userPhoto = document.getElementById("userPhoto");
+        userPhoto.src = data.avatar_url;
+    });
+}
+
+getInformationsGithub();
+
+// TODO: corrigir background
